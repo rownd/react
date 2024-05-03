7b14b700a81ddf7b64fbcc6bca740a55655d507c
Bobby Radford <bobby@rownd.io>
2024-05-03 10:11:14 -0400

    feat: add environment to example app (#9)

---
+++ examples/react-router/src/environment.tsx
@@ -0,0 +1,5 @@
+export const environment = {
+  API_URL: import.meta.env.VITE_APP_API_URL,
+  HUB_BASE_URL: import.meta.env.VITE_APP_HUB_BASE_URL,
+  ROWND_APP_KEY: import.meta.env.VITE_APP_ROWND_APP_KEY,
+}
--- examples/react-router/src/main.tsx
+++ examples/react-router/src/main.tsx
@@ -4,10 +4,16 @@ import './index.css'
 
 import { RowndProvider } from '../../../src/index.tsx'
 import Router from './router/router.tsx'
+import { environment } from './environment.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RowndProvider appKey='YOUR APP KEY'>
+    <RowndProvider 
+      apiUrl={environment.API_URL}
+      hubUrlOverride={environment.HUB_BASE_URL}
+      appKey={environment.ROWND_APP_KEY}
+    >
       <Router />
     </RowndProvider>
   </React.StrictMode>,
