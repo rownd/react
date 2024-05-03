3e8fd294bb59d80422cb5c2e899602df25c8f79c
mfmurray <mfmurray@umich.edu>
2024-05-03 11:12:12 -0400

    feat: add random name generator

--- examples/react-router/src/router/welcome/Welcome.tsx
+++ examples/react-router/src/router/welcome/Welcome.tsx
@@ -1,13 +1,14 @@
 import './Welcome.css';
 import { useRownd } from '@rownd/react';
 import { Link } from 'react-router-dom';
+import { adjectives, uniqueNamesGenerator, colors, animals } from 'unique-names-generator';
 
 function Welcome() {
-  const { is_initializing, is_authenticated, auth_level, user } = useRownd();
+  const { is_initializing, is_authenticated, auth_level, user, setUserValue } = useRownd();
 
   return (
     <>
-      <h1>Welcome</h1>
+      <h1>Welcome {user.data.unique_name || 'new person'}</h1>
       {is_initializing && <h2>Initializing...</h2>}
       {user.instant_user?.is_initializing && (
         <h2>Initializing instant user...</h2>
@@ -18,6 +19,16 @@ function Welcome() {
           <h2>Auth level: {auth_level}</h2>
         </>
       )}
+      <h2>{'Random name generator'}</h2>
+      <button onClick={() => {
+        const randomName= uniqueNamesGenerator({
+          dictionaries: [adjectives, colors, animals]
+        }); 
+        setUserValue('unique_name', randomName)
+        console.log({ randomName })
+      }}>Generate new name</button>
+      <br />
+      <br />
       <Link to={'/dashboard'}>Go to Dashboard</Link>
     </>
   );
