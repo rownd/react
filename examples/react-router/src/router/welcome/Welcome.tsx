c257e63cbf3fdb2b5c8655882eb14712078322f4
mfmurray <mfmurray@umich.edu>
2024-05-03 11:44:36 -0400

    feat: add random email

--- examples/react-router/src/router/welcome/Welcome.tsx
+++ examples/react-router/src/router/welcome/Welcome.tsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 import { adjectives, uniqueNamesGenerator, colors, animals } from 'unique-names-generator';
 
 function Welcome() {
-  const { is_initializing, is_authenticated, auth_level, user, setUserValue } = useRownd();
+  const { is_initializing, is_authenticated, auth_level, user, setUser} = useRownd();
 
   return (
     <>
-      <h1>Welcome {user.data.unique_name || 'new person'}</h1>
+      <h1>Welcome: {user.data.unique_name || 'new person'}</h1>
       {is_initializing && <h2>Initializing...</h2>}
       {user.instant_user?.is_initializing && (
         <h2>Initializing instant user...</h2>
@@ -24,7 +24,11 @@ function Welcome() {
         const randomName= uniqueNamesGenerator({
           dictionaries: [adjectives, colors, animals]
         }); 
-        setUserValue('unique_name', randomName)
+        setUser({
+          ...user.data,
+          unique_name: randomName.split('_').join(' '),
+          email: `${crypto.randomUUID().slice(0,10)}@random.com`,
+        })
         console.log({ randomName })
       }}>Generate new name</button>
       <br />
