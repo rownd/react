f624ac8efcbdae96d22a794b440c7550553bca14
mfmurray <mfmurray@umich.edu>
2024-05-06 16:16:38 -0400

    feat: improve react-router-example

--- examples/react-router/src/router/dashboard/Dashboard.tsx
+++ examples/react-router/src/router/dashboard/Dashboard.tsx
@@ -9,7 +9,6 @@ const Dashboard: React.FC = () => {
       <h1>Dashboard</h1>
       {is_authenticated && (
         <>
-          <h2>Authenticated</h2>
           <h2>Auth level: {auth_level}</h2>
         </>
       )}
--- examples/react-router/src/router/welcome/Welcome.tsx
+++ examples/react-router/src/router/welcome/Welcome.tsx
@@ -1,36 +1,22 @@
 import './Welcome.css';
 import { useRownd } from '@rownd/react';
 import { Link } from 'react-router-dom';
-import { adjectives, uniqueNamesGenerator, colors, animals } from 'unique-names-generator';
 
 function Welcome() {
-  const { is_initializing, is_authenticated, auth_level, user, setUser} = useRownd();
+  const { is_initializing, is_authenticated, auth_level, user } = useRownd();
 
   return (
     <>
-      <h1>Welcome: {user.data.unique_name || 'new person'}</h1>
+      <h1>Welcome: {`${user.data.first_name ?? ''} ${user.data.last_name ?? ''}`}</h1>
       {is_initializing && <h2>Initializing...</h2>}
-      {user.instant_user?.is_initializing && (
-        <h2>Initializing instant user...</h2>
-      )}
       {is_authenticated && (
         <>
-          <h2>Authenticated</h2>
           <h2>Auth level: {auth_level}</h2>
         </>
       )}
-      <h2>{'Random name generator'}</h2>
-      <button onClick={() => {
-        const randomName= uniqueNamesGenerator({
-          dictionaries: [adjectives, colors, animals]
-        }); 
-        setUser({
-          ...user.data,
-          unique_name: randomName.split('_').join(' '),
-          email: `${crypto.randomUUID().slice(0,10)}@random.com`,
-        })
-        console.log({ randomName })
-      }}>Generate new name</button>
+      <button id="request-info" onClick={() => {}}>
+        Add/Edit name
+      </button>
       <br />
       <br />
       <Link to={'/dashboard'}>Go to Dashboard</Link>
