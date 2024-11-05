20d0f8355bc4670fb066450c86d310fc6e6a250e
Michael Murray <mfmurray@umich.edu>
2024-11-05 13:01:27 -0500

    feat: add onUnauthenticated callback for remix hof

--- src/remix/server/withRowndLoader.tsx
+++ src/remix/server/withRowndLoader.tsx
@@ -5,20 +5,17 @@ type LoaderFunctionArgs = {
   context: Record<string, any>;
 } & any;
 
-type RowndLoaderResponse =
-  | {
-      access_token: string;
-      is_authenticated: true;
-      user_id: string;
-    }
-  | {
-      access_token: undefined;
-      is_authenticated: false;
-      user_id: undefined;
-    };
+type RowndLoaderResponse = {
+  access_token: string;
+  is_authenticated: true;
+  user_id: string;
+};
 
 export const withRowndLoader = (
-  loader: (args: LoaderFunctionArgs, rowndResponse: RowndLoaderResponse) => Promise<any>
+  loader: (args: LoaderFunctionArgs, rowndResponse: RowndLoaderResponse) => Promise<any>,
+  options: {
+    onUnauthenticated?: (args: LoaderFunctionArgs) => void;
+  } = {}
 ) => {
   return async function (args: LoaderFunctionArgs) {
     const { request } = args;
@@ -27,6 +24,10 @@ export const withRowndLoader = (
     );
 
     if (!status.is_authenticated) {
+      const onUnauthenticatedCallback = options?.onUnauthenticated;
+      if (onUnauthenticatedCallback) {
+        return onUnauthenticatedCallback(args);
+      }
       return { is_authenticated: false };
     }
     return loader(args, {
