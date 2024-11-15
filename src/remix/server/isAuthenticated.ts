ff5b8c54adc9593a6493e7753355e7291c6cf42b
Michael Murray <mfmurray@umich.edu>
2024-11-15 09:25:26 -0500

    fix: boolean type

--- src/remix/server/isAuthenticated.ts
+++ src/remix/server/isAuthenticated.ts
@@ -1,6 +1,6 @@
 import { getRowndAuthenticationStatus } from '../../ssr/server/token';
 
-export const isAuthenticated = async (request: Request): Promise<Boolean> => {
+export const isAuthenticated = async (request: Request): Promise<boolean> => {
   const status = await getRowndAuthenticationStatus(
     request.headers.get('Cookie')
   );
