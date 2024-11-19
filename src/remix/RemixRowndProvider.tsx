429a64f26b40ffe1361c8b4ca18daeb544e3e856
Michael Murray <mfmurray@umich.edu>
2024-11-19 16:02:57 -0500

    feat: add onAuthenticated callback to useRownd

--- src/remix/RemixRowndProvider.tsx
+++ src/remix/RemixRowndProvider.tsx
@@ -1,17 +1,38 @@
 import React, { lazy, Suspense } from 'react';
-import { RowndProvider, RowndProviderProps } from '../context/RowndProvider';
-const InternalProviderHubScriptInjector = lazy(() => import('../context/HubScriptInjector/InternalProviderHubScriptInjector'));
+import { RowndContext, RowndProviderProps } from '../context/RowndContext';
+import useHub from '../hooks/useHub';
+import { TRowndContext } from '../context/types';
+import { InternalRowndProvider } from '../context/InternalProvider';
+const InternalProviderHubScriptInjector = lazy(
+  () => import('../context/HubScriptInjector/InternalProviderHubScriptInjector')
+);
 const RemixClientScript = lazy(() => import('./client/RemixClientScript'));
 
 function RemixRowndProvider({ children, ...props }: RowndProviderProps) {
+  const { setInitialHubState, hubListenerCb } = useHub();
+
+  const [hubState, setHubState] = React.useState<TRowndContext>({
+    ...setInitialHubState(),
+    onAuthenticated: () => {
+      return () => void 0;
+    },
+  });
+
   return (
-    <RowndProvider {...props}>
-      <Suspense fallback={null}>
-        <InternalProviderHubScriptInjector />
-        <RemixClientScript />
-      </Suspense>
-      {children}
-    </RowndProvider>
+    <InternalRowndProvider
+      stateListener={({ state, api }) =>
+        hubListenerCb({ state, api, callback: setHubState })
+      }
+      {...props}
+    >
+      <RowndContext.Provider value={hubState}>
+        <Suspense fallback={null}>
+          <InternalProviderHubScriptInjector />
+          <RemixClientScript />
+        </Suspense>
+        {children}
+      </RowndContext.Provider>
+    </InternalRowndProvider>
   );
 }
 
