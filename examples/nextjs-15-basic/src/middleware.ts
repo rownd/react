dc057182346b6cb29ccdbf4cd1ef1cd5cb5b5883
Matt Hamann <mhamann@rownd.io>
2025-01-13 14:03:45 -0500

    chore: more consistent example naming

--- examples/nextjs-15-basic/src/middleware.ts
+++ examples/nextjs-15-basic/src/middleware.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from 'next/server';
 import { withRowndMiddleware } from '@rownd/next/server';
 
-export const middleware = withRowndMiddleware(() => {
+export const middleware = withRowndMiddleware((req) => {
+  if (req.user) {
+    console.log('User is authenticated');
+  }
+
   return NextResponse.next();
 });
 
@@ -9,5 +13,6 @@ export const config = {
   matcher: [
     // Match API routes
     '/api/rownd-token-callback',
+    '/:path*',
   ],
 };
